b989534d4895a41d3b2296f03a2e96eb5160ec26
Naiteek11 <naiteekpb@gmail.com>
2026-08-25 10:42:13 +0530

    Add Number class to find maximum of two numbers

---
+++ Practical 6/prog1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Number
+{
+    int a, b;
+public:
+    void getData()
+    {
+    cout << "Enter two numbers: ";
+     cin >> a >> b;
+    }
+    friend void findMax(Number n);
+};
+void findMax(Number n)
+{
+    if (n.a > n.b)
+      cout << "Maximum number = " << n.a;
+    else
+      cout << "Maximum number = " << n.b;
+}
+int main()
+{
+    Number n;
+    n.getData();
+    findMax(n);
+    return 0;
+}
