b7d3c39d9097127a2ebcf7556c68775d0b17a359
Naiteek11 <naiteekpb@gmail.com>
2026-08-25 10:38:51 +0530

    Add Circle class to calculate area from radius

---
+++ Practical 5/prog3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Circle
+{
+public:
+    float r;
+
+    void accept()
+    {
+        cout << "Enter radius: ";
+        cin >> r;
+    }
+
+    inline void display()
+    {
+        cout << "Area = " << 3.14 * r * r;
+    }
+};
+
+int main()
+{
+    Circle c;
+
+    c.accept();
+    c.display();
+
+    return 0;
+}
