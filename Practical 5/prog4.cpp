361a050691a7192643591db09d58f22e22df59d3
Naiteek11 <naiteekpb@gmail.com>
2026-08-25 10:39:28 +0530

    Add Simple Interest calculator

---
+++ Practical 5/prog4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class SI
+{
+public:
+    float p, r, t;
+
+    void accept()
+    {
+        cout << "Enter Principal, Rate and Time: ";
+        cin >> p >> r >> t;
+    }
+
+    inline void display()
+    {
+        cout << "Simple Interest = " << (p * r * t) / 100;
+    }
+};
+
+int main()
+{
+    SI s;
+
+    s.accept();
+    s.display();
+
+    return 0;
+}
