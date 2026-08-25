0c82c5c4c110f5dab5699d2fe019721890098008
Naiteek11 <naiteekpb@gmail.com>
2026-08-25 10:42:42 +0530

    Implement classes A and B with max function

---
+++ Practical 6/prog2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int a;
+public:
+    A(int x) { a = x; }
+    int getA() { return a; }
+};
+
+class B
+{
+    int b;
+public:
+    B(int x) { b = x; }
+    friend void max(A, B);
+};
+
+void max(A x, B y)
+{
+    if(x.getA() > y.b)
+        cout << "Maximum = " << x.getA();
+    else
+        cout << "Maximum = " << y.b;
+}
+
+int main()
+{
+    A x(10);
+    B y(20);
+    max(x, y);
+  return 0;
+}
